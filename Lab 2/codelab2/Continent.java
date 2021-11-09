6d2e14f021d6145f2287bd3bd6856e766b00ff68
u186412 <92033402+u186412@users.noreply.github.com>
2021-11-09 12:25:45 +0100

    Add files via upload

---
+++ Lab 2/codelab2/Continent.java
@@ -0,0 +1,27 @@
+import java.util.LinkedList;
+
+public class Continent {
+    //Attribute//
+    private LinkedList<PolygonalRegion> countries;
+
+    //Methods//
+    //Constructor//
+    public Continent(LinkedList<PolygonalRegion> c){
+        this.countries = c;
+    }
+
+    //getter//
+    public double getTotalArea(){
+        double Area = 0
+        for(int i = 0; i <= this.countries.size(); i++ ){
+            Area += this.countries.get(i).getArea();
+        }
+        return Area;
+    }
+    public void draw(java.awt.Graphics g){
+        for(int i = 0; i <= this.countries.size(); i++){
+            this.countries.get(i).draw(g);
+        }
+    }
+    
+}
